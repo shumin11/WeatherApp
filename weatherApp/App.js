f72e82b33a3a74b5a5b4eb0406dae6a62f16ea25
emmahy-park <68018787+emmahy-park@users.noreply.github.com>
2023-03-25 09:55:53 -0700

    Update App.js

--- weatherApp/App.js
+++ weatherApp/App.js
@@ -1,33 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React, {useState, useEffect} from 'react';
 
 export default function App() {
-  const [quote, setQuote] = useState("")
+  // weatherData holds the weather data of the current location
+  // processWeatherData is a function that updates weatherData
+  const [weatherData, processWeatherData] = useState(0); // initialize to 0 degrees 
 
-  const randomQuote = () => {
-    fetch("https://api.quotable.io/random")
-    .then(res => res.json())
-    .then(result => {
+  const getweatherDataFromApi = () => {
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=49.25&longitude=-123.12&hourly=weatherData_2m,precipitation")
+    .then(response => {
+      if (!response.ok) {
+        throw response; // check the http response code and if isn't ok then throw the response as an error
+      }
+      return response.json(); // parse the result as JSON
+    }).then(result => {
       console.log(result)
-      setQuote(result.content)
-    })
+      processWeatherData(result.content)
+    }).catch((errorResponse) => {
+      if (errorResponse.text) {
+        errorResponse.text().then (errorMessage => {
+          // errorMessage
+        })
+      } else {
+        // no additional error information
+      }
+    });
   }
 
-  useEffect = (()=> {
-    randomQuote()
-  });
+  useEffect(() => {
+    getweatherDataFromApi();
+  }, [])
 
   return (
     <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <View style={styles.quoteContainer}>
-       <Text style = {styles.titleText}>Quote of the day</Text>
-       <Text style = {styles.quoteText}>{quote}</Text>
-       <TouchableOpacity onPress = {randomQuote} style= {styles.buttonStyle}>
-         <Text style = {styles.buttonText}>New Quote</Text>
-       </TouchableOpacity>
-      </View>
     </View>
   );
 }
@@ -35,41 +43,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#5372F0',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  quoteContainer: {
-    width: '90%',
-    backgroundColor: '#fff',
-    borderRadius: 20,
-    padding: 20
-  },
-  titleText: {
-    textAlign: 'center',
-    color: '#333',
-    fontSize: 26,
-    fontWeight: 600,
-    marginBottom: 20
-  },
-  quoteText: {
-    color: '#000',
-    fontSize: 16,
-    lineHeight: 26,
-    letterSpacing: 1.1,
-    fontWeight: 400,
-    textAlign: 'center',
-    marginBottom: 10
-  },
-  buttonStyle: {
-    backgroundColor: '#5372F0',
-    padding: 20,
-    borderRadius: 30,
-    marginVertical: 20
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 18,
-    textAlign: 'center'
-  }
 });
