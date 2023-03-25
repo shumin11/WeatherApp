9bacbcf3fd41f4a435fd66b75a831825254e24fb
shumin11 <wsmin123456@hotmail.com>
2023-03-25 09:31:58 -0700

    test app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # WeatherApp
+expo start --web
--- weatherApp/App.js
+++ weatherApp/App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
 
 export default function App() {
+  const [quote, setQuote] = useState("")
+
+  const randomQuote = () => {
+    fetch("https://api.quotable.io/random")
+    .then(res => res.json())
+    .then(result => {
+      console.log(result)
+      setQuote(result.content)
+    })
+  }
+
+  useEffect = (()=> {
+    randomQuote()
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.quoteContainer}>
+       <Text style = {styles.titleText}>Quote of the day</Text>
+       <Text style = {styles.quoteText}>{quote}</Text>
+       <TouchableOpacity onPress = {randomQuote} style= {styles.buttonStyle}>
+         <Text style = {styles.buttonText}>New Quote</Text>
+       </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +35,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5372F0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  quoteContainer: {
+    width: '90%',
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    padding: 20
+  },
+  titleText: {
+    textAlign: 'center',
+    color: '#333',
+    fontSize: 26,
+    fontWeight: 600,
+    marginBottom: 20
+  },
+  quoteText: {
+    color: '#000',
+    fontSize: 16,
+    lineHeight: 26,
+    letterSpacing: 1.1,
+    fontWeight: 400,
+    textAlign: 'center',
+    marginBottom: 10
+  },
+  buttonStyle: {
+    backgroundColor: '#5372F0',
+    padding: 20,
+    borderRadius: 30,
+    marginVertical: 20
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    textAlign: 'center'
+  }
 });
